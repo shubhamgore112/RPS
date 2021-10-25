3808c66f7295b3b1be730bacd9c3219262b54b60
Shubham <shubhamgore112@gmail.com>
2021-10-25 21:10:50 +0530

    Files Updated,UI Added

--- script/script.js
+++ script/script.js
@@ -1,63 +1,91 @@
-function computerPlay(){
+// Cash the DOM
+let computerScore=0;
+let userScore=0;
+const scoreboard_div=document.querySelector(".scoreboard");
+const scoreinfo_class= document.querySelector(".score-info");
+const choiceContainer_div= document.querySelector(".choice-container");
+const choice_div= document.querySelector(".choice");
+const result_div= document.querySelector(".result");
+const userLabel_id= document.getElementById("user-label");
+const computerLabel_id= document.getElementById("computer-label");
+const rock_id= document.getElementById("r");
+const paper_id= document.getElementById("p");
+const scissor_id= document.getElementById("s");
+const score_span= document.querySelector("score");
+const userScore_span=document.getElementById("user-score");
+const computerScore_span=document.getElementById("computer-score");
+
+function getComputerPlay(){
     var choices=["rock","paper","scissor"];
     var computerChoice=Math.floor(Math.random()*choices.length);
     return choices[computerChoice];
-    // console.log(choices[computerChoice],computerChoice);
 }
-function userPlay(){
-    var userChoice = (window.prompt("Enter your Choice: ")).toLowerCase();
-    return userChoice;
+function main(){
+
+    const paper_result=paper_id.addEventListener('click',function() {
+       game("paper");
+    });
+    const rock_result= rock_id.addEventListener('click',function() {
+        game('rock');
+    });
+    const scissor_result= scissor_id.addEventListener('click', function() {
+        game('scissor');
+    });
 }
 
-// var computerCurrentChoice=computerPlay();
-// var userCurrentChoice=userPlay();
+function win(user,computer){
+    userScore++ ;
 
-function check(computer,user){
+    const smallUserWord = "user".fontsize(1).sup();
+    const smallCompWord = "comp".fontsize(1).sup();
+
+    userScore_span.innerHTML=userScore;
+    computerScore_span.innerHTML=computerScore;
+    result_div.innerHTML=`${user.charAt(0).toUpperCase()+user.slice(1)}${smallUserWord.sub()} beats ${computer.charAt(0).toUpperCase()+computer.slice(1)}${smallCompWord.sub()}, You Win! `;
+
+
+}
+
+function lose(user,computer){
+    computerScore++ ;
+
+    const smallUserWord = "user".fontsize(1).sup();
+    const smallCompWord = "comp".fontsize(1).sup();
+
+    computerScore_span.innerHTML=computerScore;
+    userScore_span.innerHTML=userScore;
+    result_div.innerHTML=`${computer.charAt(0).toUpperCase()+computer.slice(1)}${smallCompWord.sub()} beats ${user.charAt(0).toUpperCase()+user.slice(1)}${smallUserWord.sub()}, You lose. `;
+
+}
+
+function draw(user,computer){
+    
+    const smallUserWord = "user".fontsize(1).sup();
+    const smallCompWord = "comp".fontsize(1).sup();
+    
+    computerScore_span.innerHTML=computerScore;
+    userScore_span.innerHTML=userScore;
+    result_div.innerHTML=`${computer.charAt(0).toUpperCase()+computer.slice(1)}${smallCompWord.sub()} and ${user.charAt(0).toUpperCase()+user.slice(1)}${smallUserWord.sub()}, It's DRAW. `;
+}
+
+
+
+function game(main){
+
+    const user= main;
+    const computer= getComputerPlay();
+
     if (computer==user){
-        return "Match tie";
+        draw(user,computer);
+    
     }else if((computer=="paper" && user=="rock")||(computer=="rock" && user=="scissor")||(computer=="scissor" && user=="paper")){
-        return "Computer Wins";
+        lose(user,computer);
+    
     }else if((computer=="paper" && user=="scissor")||(computer=="rock" && user=="paper")||(computer=="scissor" && user=="rock")){
-        return "You Win";
-    // }else if(computer=="paper" && user=="scissor"){
-    //     return "Scissor tears paper,You Win";
-    // }else if(computer=="rock" && user=="paper"){
-    //     return "Paper captures Rock, You Win";
-    // }else if(computer=="rock" && user=="scissor"){
-    //     return "Rock breaks Scissor, Computer Wins";
-    // }else if(computer=="scissor" && user=="paper"){
-    //     return "Scissor tears Paper, Computer Win";
-    // }else if(computer=="scissor" && user=="rock"){
-    //     return "Rock breaks Scissor, You Win";
-    // }else return "invalid";
-    }else return "Invalid Choice";
+        win(user,computer);
+
+    }else console.log("Invalid Choice");
 }
 
-// function game(n){
-//     while (n>0){
-//         console.log(check(computerCurrentChoice,userCurrentChoice));
-//         n=n-1;
-//     }
-// }
-// game(5);
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(computerPlay());
-// console.log(check(computerCurrentChoice,userCurrentChoice));
+main();
 
-function game(i){
-    while (i>0){
-        let userCurrentPlay=userPlay();
-        let computerCurrentPlay=computerPlay();
-        console.log(check(computerCurrentPlay,userCurrentPlay));
-        i-=1;
-
-    }
-}
-game(4);
