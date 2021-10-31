8973ab0a9469c1ee549c49eec374bc36b9620dd7
Shubham <shubhamgore112@gmail.com>
2021-10-31 18:15:12 +0530

    One who scores 5 points first, wins the game.

--- script/script.js
+++ script/script.js
@@ -1,19 +1,14 @@
 // Cash the DOM
 let computerScore=0;
 let userScore=0;
-const scoreboard_div=document.querySelector(".scoreboard");
-const scoreinfo_class= document.querySelector(".score-info");
-const choiceContainer_div= document.querySelector(".choice-container");
-const choice_div= document.querySelector(".choice");
+
 const result_div= document.querySelector(".result");
-const userLabel_id= document.getElementById("user-label");
-const computerLabel_id= document.getElementById("computer-label");
 const rock_id= document.getElementById("r");
 const paper_id= document.getElementById("p");
 const scissor_id= document.getElementById("s");
-const score_span= document.querySelector("score");
 const userScore_span=document.getElementById("user-score");
 const computerScore_span=document.getElementById("computer-score");
+
 
 function getComputerPlay(){
     var choices=["rock","paper","scissor"];
@@ -38,11 +33,13 @@ function win(user,computer){
 
     const smallUserWord = "user".fontsize(1).sup();
     const smallCompWord = "comp".fontsize(1).sup();
-
     userScore_span.innerHTML=userScore;
     computerScore_span.innerHTML=computerScore;
     result_div.innerHTML=`${user.charAt(0).toUpperCase()+user.slice(1)}${smallUserWord.sub()} beats ${computer.charAt(0).toUpperCase()+computer.slice(1)}${smallCompWord.sub()}, You Win! `;
+    if (userScore==5){
+        stop("WON");
 
+    }
 
 }
 
@@ -51,28 +48,32 @@ function lose(user,computer){
 
     const smallUserWord = "user".fontsize(1).sup();
     const smallCompWord = "comp".fontsize(1).sup();
-
     computerScore_span.innerHTML=computerScore;
     userScore_span.innerHTML=userScore;
     result_div.innerHTML=`${computer.charAt(0).toUpperCase()+computer.slice(1)}${smallCompWord.sub()} beats ${user.charAt(0).toUpperCase()+user.slice(1)}${smallUserWord.sub()}, You lose. `;
+    if (computerScore==5){
+        stop("LOSE");
+    }
 
 }
 
-function draw(user,computer){
-    
+function draw(user,computer){ 
     const smallUserWord = "user".fontsize(1).sup();
     const smallCompWord = "comp".fontsize(1).sup();
-    
     computerScore_span.innerHTML=computerScore;
     userScore_span.innerHTML=userScore;
     result_div.innerHTML=`${computer.charAt(0).toUpperCase()+computer.slice(1)}${smallCompWord.sub()} and ${user.charAt(0).toUpperCase()+user.slice(1)}${smallUserWord.sub()}, It's DRAW. `;
 }
 
+function stop(winner){
+    userScore=0;
+    computerScore=0;
+    result_div.innerHTML=`YOU ${winner}, Click on Rock, Paper OR Scissor icons to play again. `;
+
+}
 
 
-function game(main){
-
-    const user= main;
+function game(user){
     const computer= getComputerPlay();
 
     if (computer==user){
